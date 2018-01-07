b1616a5c93c728ad91b85f54214aa6aaafb9f925
Hayden Carlson <hayden.carlson@icloud.com>
2018-01-06 23:08:20 -0800

    Add services index to view differen't services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,12 +1,16 @@
 class ServicesController < ApplicationController
 
   def new
+    if !params['service_id']
+      return redirect_to services_path
+    end
+    @service = Service::Item.find(params['service_id'])
     @subscription = Subscription.new
     @service_types = ['PayPal', 'Bitcoin', 'Ethereum']
   end
 
   def index
-    # @tv_services = Service.where(category: "TV")
+    @service_categories = Service::Category.all
   end
 
 end
--- app/models/service/item.rb
+++ app/models/service/item.rb
@@ -1,3 +1,3 @@
 class Service::Item < ApplicationRecord
-  belongs_to :service_category, :class_name => "Service::Category"
+  belongs_to :service_category, :class_name => "Service::Category", :foreign_key => "service_category_id"
 end
