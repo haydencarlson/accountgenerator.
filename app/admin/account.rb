85aa448b8dc53536e82452c4a36f16f6b83ea3af
Hayden Carlson <hayden.carlson@icloud.com>
2018-01-09 23:21:22 -0800

    Add account importer

--- app/admin/account.rb
+++ app/admin/account.rb
@@ -1,6 +1,18 @@
 ActiveAdmin.register Account do
   permit_params :login, :password, :service_item_id, :country_iso_4217, :status
 
-  menu parent: "Accounts"
+  controller do
+    def import_accounts
+      renders view 'app/views/admin/accounts/import_accounts.html.erb'
+    end
+  end
+
+  action_item do
+    link_to "Import Accounts", import_accounts_admin_accounts_path
+  end
+
+  collection_action :import_accounts, :method => :get do
+
+  end
 
 end
